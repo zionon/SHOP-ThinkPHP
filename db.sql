4e1965e1ab51bab1e1844dc3ba2ff11e15fa5d89
gh-huang <gh_huang@outlook.com>
2016-06-02 13:38:08 +0800

    创建数据库表

---
+++ db.sql
@@ -0,0 +1,22 @@
+drop database if exists shop_thinkphp;
+create database shop_thinkphp;
+use shop_thinkphp;
+set names utf8;
+
+
+drop table if exists st_goods;
+create table st_goods
+(
+	id mediumint unsigned not null auto_increment comment 'Id',
+	goods_name varchar(150) not null comment '商品名称',
+	market_price decimal(10,2) not null comment '市场价格',
+	shop_price decimal(10,2) not null comment '本店价格',
+	goods_desc longtext comment '商品描述',
+	is_on_sale enum('是','否') not null default '是' comment '是否上架',
+	is_delete enum('是','否') not null default '否' comment '是否放到回收站',
+	addtime datetime not null comment '添加时间',
+	primary key (id),
+	key shop_price(shop_price),
+	key addtime(addtime),
+	key is_on_sale(is_on_sale)
+)engine=InnoDB default charset=utf8 comment '商品';
