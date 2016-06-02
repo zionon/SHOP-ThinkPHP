c19305b15c46c643ac8628a62deceef65576f1ea
gh-huang <gh_huang@outlook.com>
2016-06-02 12:40:18 +0800

    后台框架初步搭建

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,27 @@
+<?php
+//后台首页控制器
+//命名空间
+namespace Admin\Controller;
+use \Think\Controller;
+
+class IndexController extends Controller{
+	//后台主页框架显示
+	public function index() {
+		$this->display();
+	}
+
+	//后台头部显示
+	public function top() {
+		$this->display();
+	}
+
+	//后台菜单栏显示
+	public function menu() {
+		$this->display();
+	}
+
+	//后台详情显示
+	public function main() {
+		$this->display();
+	}
+}
