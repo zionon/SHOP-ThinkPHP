f84be16ffac12f25a77f32b5b3bf9bdd162560a9
gh-huang <gh_huang@outlook.com>
2016-06-07 15:49:14 +0800

    建表，商品属性管理

--- db.sql
+++ db.sql
@@ -22,6 +22,7 @@ create table st_goods
 	mbig_logo varchar(150) not null default '' comment '更大图',
 	brand_id mediumint unsigned not null default '0' comment '品牌id',
 	cat_id mediumint unsigned not null default '0' comment '主分类Id',
+	type_id mediumint unsigned not null default '0' comment '类型Id',
 	primary key (id),
 	key shop_price(shop_price),
 	key addtime(addtime),
@@ -82,7 +83,7 @@ create table st_category
 	primary key (id)
 )engine=InnoDB default charset=utf8 comment '分类';
 
-drop table if exists st_goods_cat
+drop table if exists st_goods_cat;
 create table st_goods_cat
 (
 	cat_id mediumint unsigned not null comment '分类id',
@@ -91,6 +92,37 @@ create table st_goods_cat
 	key cat_id(cat_id)
 )engine=InnoDB default charset=utf8 comment '商品拓展分类';
 
+drop table if exists st_type;
+create table st_type
+(
+	id mediumint unsigned not null auto_increment comment 'Id',
+	type_name varchar(30) not null comment '类型名称',
+	primary key(id)
+)engine=InnoDB default charset=utf8 comment '类型';
+
+drop table if exists st_attribute;
+create table st_attribute
+(
+	id mediumint unsigned not null auto_increment comment 'Id',
+	attr_name varchar(30) not null comment '属性名称',
+	attr_type enum('唯一','可选') not null comment '属性类型',
+	attr_option_values varchar(300) not null default '' comment '属性可选值',
+	type_id mediumint unsigned not null comment '所属类型Id',
+	primary key (id),
+	key type_id(type_id)
+)engine=InnoDB default charset=utf8 comment '属性表';
+
+drop table if exists st_goods_attr;
+create table st_goods_attr
+(
+	id mediumint unsigned not null auto_increment comment 'Id',
+	attr_value varchar(150) not null default '' comment '属性值',
+	attr_id mediumint unsigned not null comment '属性Id',
+	goods_id mediumint unsigned not null comment '商品Id',
+	primary key (id),
+	key goods_id(goods_id),
+	key attr_id(attr_id)
+)engine=InnoDB default charset=utf8 comment '商品属性';
 
 
 
