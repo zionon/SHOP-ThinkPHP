87265662be1979cad1bea5454aa7bdd55c02b8d9
gh-huang <gh_huang@outlook.com>
2016-06-13 16:27:58 +0800

    开启首页缓存

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,11 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'HTML_CACHE_ON' => true,	//开启静态缓存
+	'HTML_CACHE_TIME' => 60,	//全局静态缓存有效期（秒）
+	'HTML_FILE_SUFFIX' => '.shtml',	//设置静态缓存文件的后缀
+	//这个模块中哪些页面生成静态页
+	'HTML_CACHE_RULES' => array(
+		'index:index' => array('index',86400),	//首页生成index.shtml一天
+		),
 );
