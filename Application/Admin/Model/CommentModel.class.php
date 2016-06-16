2a73a08d9bf0a0196ca60fd24c4b12df8262e891
gh-huang <gh_huang@outlook.com>
2016-06-16 23:15:01 +0800

    AJAX回复评论

--- Application/Admin/Model/CommentModel.class.php
+++ Application/Admin/Model/CommentModel.class.php
@@ -25,4 +25,37 @@ class CommentModel extends Model{
 		$data['member_id'] = $memberId;
 		$data['addtime'] = date('Y-m-d H:i:s');
 	}
+
+	public function search ($goodsId, $pageSize = 5) {
+		//这里因为要做AJAX翻页，所以我们需要自己做翻页不能用TP自带的，自带的不是AJAX的，一点击就跳转了
+		$where['a.goods_id'] = array('eq', $goodsId);
+
+		//取出总的记录数
+		$count = $this->alias('a')->where($where)->count();
+		//计算总的页数
+		$pageCount = ceil($count / $pageSize);
+		//获取当前页
+		$currentPage = max(1, (int)I('get.p'));		// >= 1的整数
+		//计算limit上的第一次参数，偏移量
+		$offset = ($currentPage - 1) * $pageSize;
+
+		//取数据
+		$data = $this->alias('a')
+		->field('a.content,a.addtime,a.star,a.click_count,b.face,b.username,COUNT(c.id) reply_count')
+		->join('LEFT JOIN __MEMBER__ b ON a.member_id=b.id
+			 	LEFT JOIN __COMMENT_REPLY__ c ON a.id=c.comment_id')
+		->where($where)
+		->order('a.id DESC')
+		->limit("$offset,$pageSize")
+		->group('a.id')
+		->select();
+
+		return array(
+			'data' => $data,
+			'pageCount' => $pageCount,
+		);
+
+	}
 }
+
+
