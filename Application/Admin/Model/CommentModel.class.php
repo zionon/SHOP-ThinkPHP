edce35e7e683cedfc5cdfe223f2be44b5acba392
gh-huang <gh_huang@outlook.com>
2016-06-17 11:35:36 +0800

    显示评价

--- Application/Admin/Model/CommentModel.class.php
+++ Application/Admin/Model/CommentModel.class.php
@@ -39,6 +39,34 @@ class CommentModel extends Model{
 		//计算limit上的第一次参数，偏移量
 		$offset = ($currentPage - 1) * $pageSize;
 
+		if ($currentPage == 1) {
+			//好评率
+			//取出所有的分值
+			$stars = $this->field('star')->where(array('goods_id'=>array('eq',$goodsId)))->select();
+			//循环所有的分值进行统计
+			$hao = $zhong = $cha = 0;
+			foreach ($stars as $k => $v) {
+				if ($v['star'] == 3) {
+					$zhong++;
+				} elseif ($v['star'] > 3) {
+					$hao++;
+				} else {
+					$cha++;
+				}
+			}
+			$total = $hao + $zhong + $cha; 	//总的评论数
+			$hao = round(($hao / $total) * 100, 2);
+			$zhong = round(($zhong / $total) * 100, 2);
+			$cha = round(($cha / $total) * 100, 2);
+			// dump($cha);die;
+			//取印象
+			$yxModel = D('Yinxiang');
+			$yxData = $yxModel->where(array(
+				'goods_id' => array('eq', $goodsId),
+				))->select();
+		}
+
+
 		//取数据
 		$data = $this->alias('a')
 		->field('a.content,a.addtime,a.star,a.click_count,b.face,b.username,COUNT(c.id) reply_count')
@@ -53,6 +81,10 @@ class CommentModel extends Model{
 		return array(
 			'data' => $data,
 			'pageCount' => $pageCount,
+			'hao' => $hao,
+			'zhong' => $zhong,
+			'cha' => $cha,
+			'yxData' => $yxData,
 		);
 
 	}
